ce30a40bcb40517dbeff79b16df9edc9c83757dd
RoxanneDuchesne <roxanneduchesne@gmail.com>
2020-01-14 22:09:55 -0700

    Working on Wall Jump Animation
    - Clean Up Code and File Directories

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -21,6 +21,13 @@ public class PlayerControls : PhysicsCust
     // True if wall jumping
     private bool boosting = false;
 
+    // Int equals number of wall jumps player has left
+    private int canWallJump = 1;
+
+    // Testing needs to be changed to deltatime
+    private int wallJumpTimer = 30;
+    private bool wallJumpAnimationOn = false;
+
     private SpriteRenderer spriteRenderer;
     private Animator animator;
 
@@ -44,6 +51,116 @@ public class PlayerControls : PhysicsCust
 
     }
 
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (velocity.y < 0.1f && grounded)
+        {
+            boosting = false;
+            wallJumpAnimationOn = false;
+            canWallJump = 1;
+        }
+
+        // Checks if player should wall jump
+        if (Input.GetKeyDown(action))
+        {
+            if (CanWallJump() && (canWallJump > 0))
+            {
+                WallJump();
+            }
+        }
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+            animator.SetBool("Jumped", true);
+            Debug.Log("jump");
+        }
+
+        // Allows jump cancellation
+        else if (Input.GetButtonUp("Jump"))
+        {   
+            // When boosting(walljumping)jump cancellation is stopped
+            if (velocity.y > 0 && !boosting)
+            {
+                velocity.y = velocity.y * .5f;
+                Debug.Log("jumpcancel");
+            }
+        }
+
+        UpdateFacing();
+
+        AnimatorUpdates();
+
+        targetVelocity = move * maxSpeed;
+    }
+
+    private void UpdateFacing()
+    {
+        // Ensures Sprite is Facing Correct Direction
+        if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            // Player is facing left
+            facing = false;
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            // Player is facing right
+            facing = true;
+        }
+    }
+
+    private void AnimatorUpdates()
+    {
+        animator.SetFloat("VelocityX", Math.Abs(velocity.x));
+        animator.SetBool("Grounded", grounded);
+        animator.SetFloat("SpeedY", Mathf.Abs(velocity.y) / maxSpeed);
+
+        if (velocity.y < 0.1f && grounded)
+        {
+            animator.SetBool("Jumped", false);
+            animator.SetBool("WallJumped", false);
+        }
+
+        if (wallJumpAnimationOn)
+        {
+            wallJumpTimer--;
+        }
+
+        if (wallJumpTimer == 0)
+        {
+            animator.SetBool("WallJumped", false);
+            wallJumpAnimationOn = false;
+            wallJumpTimer = 30;
+        }
+    }
+
+    private void WallJump()
+    {
+        float velocityFromJump;
+
+        // Limit Transferable Velocity From Jump 
+        if (Math.Abs(velocity.y) > (jumpTakeOffSpeed / 4))
+        {
+            velocityFromJump = (jumpTakeOffSpeed / 4);
+        }
+        else
+        {
+            velocityFromJump = Math.Abs(velocity.y);
+        }
+
+        velocity.y = velocityFromJump + wallJumpBoost * maxSpeed;
+        boosting = true;
+        wallJumpAnimationOn = true;
+        canWallJump--;
+        Debug.Log("boost");
+    }
+
     private bool CanWallJump()
     {
 
@@ -51,7 +168,7 @@ public class PlayerControls : PhysicsCust
 
         // Check which direction player is facing
         if (facing)
-        {   
+        {
             direction = Vector2.right;
             Debug.Log("right");
         }
@@ -66,80 +183,10 @@ public class PlayerControls : PhysicsCust
         if (hit.collider != null)
         {
             Debug.Log("hitwall");
+            animator.SetBool("WallJumped", true);
             return true;
         }
 
         return false;
-    }
-
-
-    protected override void ComputeVelocity()
-    {
-        Vector2 move = Vector2.zero;
-
-        move.x = Input.GetAxis("Horizontal");
-
-        if (velocity.y < 0.1f && grounded)
-        {
-            animator.SetBool("Jumped", false);
-            boosting = false;
-        }
-
-        // Checks if player should wall jump
-        if (Input.GetKeyDown(action))
-        {
-            if (CanWallJump())
-            {
-                //velocity.y = 0;
-                /*
-                if (velocity.y > 0.1){
-                    velocity.y = 1;
-                }
-                */
-
-                velocity.y = Math.Abs(velocity.y) + wallJumpBoost * maxSpeed;
-                boosting = true;
-                Debug.Log("boost");
-            }
-        }
-
-        if (Input.GetButtonDown("Jump") && grounded)
-        {
-            velocity.y = jumpTakeOffSpeed;
-            animator.SetBool("Jumped", true);
-            Debug.Log("jump");
-        }
-        // Allows jump cancellation
-        else if (Input.GetButtonUp("Jump"))
-        {   
-            // When boosting(walljumping)jump cancellation is stopped
-            if (velocity.y > 0 && !boosting)
-            {
-                velocity.y = velocity.y * .5f;
-                Debug.Log("jumpcancel");
-            }
-        }
-
-        // Ensures Sprite is Facing Correct Direction
-        if (Input.GetAxisRaw("Horizontal") < 0f)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
-            // Player is facing left
-            facing = false;
-        }
-        else if (Input.GetAxisRaw("Horizontal") > 0f)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
-            // Player is facing right
-            facing = true;
-        }
-
-
-        animator.SetFloat("VelocityX", Math.Abs(velocity.x));
-        animator.SetBool("Grounded", grounded);
-        // animator.SetFloat("velocityX", Mathf.Abs(velocity.x)/maxSpeed);
-        animator.SetFloat("SpeedY", Mathf.Abs(velocity.y)/maxSpeed);
-       // animator.SetFloat("SpeedY", velocity.y);
-        targetVelocity = move * maxSpeed;
     }
 }
