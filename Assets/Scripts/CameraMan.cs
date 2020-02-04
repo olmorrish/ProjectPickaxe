9320ef5d0a00908cf82e24972617a31eed13ab04
Oliver Morrish <olmorris@ucalgary.ca>
2020-02-03 21:19:29 -0700

    Refactored Camera Script
    Camera script changed slightly to allow us to expand in future - now has the option to toggle a boolean to follow the player.

---
+++ Assets/Scripts/CameraMan.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMan : MonoBehaviour
+{
+    public GameObject playerObj;
+
+    private bool followPlayer; //when true, will move ea frame to follow player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        followPlayer = true;
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        if (followPlayer) {
+            SnapCamera(GetPlayerLocation());
+        }
+        
+    }
+
+    private Vector2 GetPlayerLocation() {
+        return playerObj.GetComponent<Transform>().position;
+    }
+
+    void SnapCamera(Vector2 location) {
+        gameObject.GetComponent<Transform>().position = new Vector3(location.x, location.y, this.transform.position.z); //will ignore z and keep the same
+    }
+}
