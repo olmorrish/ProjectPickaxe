75497f04ce508fe608e98a5e4484b33f6be63e29
Oliver Morrish <olmorris@ucalgary.ca>
2020-02-03 21:45:20 -0700

    Added GameState
    Might work on checkpoints next.

---
+++ Assets/Scripts/GameData/GameState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    //Most Recent Checkpoint
+    //public CheckPoint currecntCheckPoint;
+    //public CheckPoint defaultCheckPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
